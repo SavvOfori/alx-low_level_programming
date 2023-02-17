0d85cacf11e9fb51fec8ac296a3e663da2d5fa46
Agyemang Ofori <savioroa@gmail.com>
2023-02-16 18:22:05 -0800

    quotess

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - shows the main function
+ * Return: 1 when successful
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
