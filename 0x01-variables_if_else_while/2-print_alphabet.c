ac9dd0dd8ab3db63414111cca93ff2391dfba410
Agyemang Ofori <savioroa@gmail.com>
2023-02-17 07:41:01 -0800

    palpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is main funtion
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
